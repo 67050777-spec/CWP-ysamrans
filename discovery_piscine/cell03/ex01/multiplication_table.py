5e266a855e4a06c533a893f674eadf463e77d1bf
pleng <67050777@kmitl.ac.th>
2026-09-23 23:57:20 +0700

    Update multiplication_table.py

--- discovery_piscine/cell03/ex01/multiplication_table.py
+++ discovery_piscine/cell03/ex01/multiplication_table.py
@@ -1,10 +1,11 @@
-try:
+#!/usr/bin/env python3
 
+try:
     number = int(input("Enter a number\n"))
-    
+
     for i in range(10):
         result = i * number
         print(f"{i} x {number} = {result}")
-        
+
 except ValueError:
     pass
